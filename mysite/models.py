30f1baa3e1088cba868f2b02aa78adfeee85d1ad
Edgar Guzman <eguzman@basecampcodingacademy.org>
2017-12-21 14:13:13 -0600

    i fixed my models.py

--- mysite/models.py
+++ mysite/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='documents/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    # document = models.FileField(upload_to='add/')
+    # uploaded_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='Instagram/static/Instagram/images')

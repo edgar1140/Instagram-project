be55047c46cac75cd3da4681c83c2567910ef552
Edgar Guzman <eguzman@basecampcodingacademy.org>
2017-12-21 14:49:55 -0600

    I finally have everything set in models.py

--- mysite/models.py
+++ mysite/models.py
@@ -3,6 +3,4 @@ from django.db import models
 
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
-    # document = models.FileField(upload_to='add/')
-    # uploaded_at = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to='Instagram/static/Instagram/images')

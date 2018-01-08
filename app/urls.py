d97418651e1e052c009574c3999e0bb30d39bf4c
Edgar Guzman <eguzman@basecampcodingacademy.org>
2018-01-08 12:46:15 -0600

    I added the url to insert the comment in my picture.

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('upload/', views.add_pic, name='upload'),
     path('feed/', views.display_pic, name='feed'),
     path('filter/<image_id>', views.PicFilter.as_view(), name='filter'),
+    path('comment/<image_id>', views.InsertComment.as_view(), name='comment')
 ]

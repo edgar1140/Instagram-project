8c9a680d82361d6173d80c2ba085f6782e25c9b6
Edgar Guzman <eguzman@basecampcodingacademy.org>
2018-01-08 14:15:06 -0600

    I modified the functions to make them work.

--- app/models.py
+++ app/models.py
@@ -4,12 +4,13 @@ from django.db import models
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     photo = models.ImageField(upload_to='app/static/Instagram/images')
+    choosing = models.ForeignKey(Topics, on_delete=models.CASCADE)
 
     def image_url(self):
         return self.photo.url[len('app/static/'):]
 
 
-# class Comment(models.Model):
-#     comment = models.CharField(max_length=220)
-#     document = models.ForeignKey(
-#         Document, on_delete=models.SET_NULL, blank=True, null=True)
+class Comment(models.Model):
+    comment = models.CharField(max_length=220)
+    document = models.ForeignKey(
+        Document, on_delete=models.SET_NULL, blank=True, null=True)
